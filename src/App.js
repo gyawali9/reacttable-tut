9fc755dfccd08a61891fd9b924e09c645ec55b58
gyawali9 <roshan.gyawali@codehimalaya.com>
2022-06-16 16:27:07 +0545

    global filtering using react table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BasicTable from "./components/BasicTable";
+import FilteringTable from "./components/FilteringTable";
 import SortingTable from "./components/SortingTable";
 
 const App = () => {
   return (
     <>
       {/* <BasicTable /> */}
-      <SortingTable />
+      {/* <SortingTable /> */}
+      <FilteringTable />
     </>
   );
 };
